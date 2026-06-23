3ead1fad01c68ef3e70033a194e15a600882a3b2
Shireen <shireenaqeel744@gmail.com>
2026-06-23 16:27:32 +0530

    first commit

---
+++ circumference.py
@@ -0,0 +1,9 @@
+import math
+
+radius= float(input("Enter the radius of the circle (in cm): "))
+circumference = 2 * math.pi * radius
+print(f"The circumference of the circle with radius {radius} cm is: {circumference:.2f} cm")
+
+#print(f"The circumference of the circle with radius {radius} cm is: {round(circumference, 2)} cm.")
+
+
---
+++ madlibs.py
@@ -0,0 +1,15 @@
+ #madlibs game
+#game where you create a story by filling in the blanks with words of your choice
+
+print("Welcome to Madlibs!")
+print("Please provide the following words to create your story.")
+adjective1 = input("Enter an adjective (describing word): ")
+noun1 = input("Enter a noun (person, place, or thing): ")
+verb1 = input("Enter a verb (action word):")
+adjective2 = input("Enter an adjective (describing word): ")
+noun2 = input("Enter a noun (person, place, or thing): ")
+verb2 = input("Enter a verb (action word):")
+adverb1 = input("Enter an adverb (describing how something is done): ")
+
+print("\nHere's your story:")
+print(f" Once there was a {adjective1} {noun1} who loved to {verb1}. One day, the {noun2} met a {adjective2} friend and they had an amazing adventure together! They decided to {verb2} {adverb1} and it was the best day ever!")
